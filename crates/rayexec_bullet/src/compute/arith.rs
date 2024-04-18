fd2e7fcb54d78c370b50ea613d5529a1e7f85f9e
Sean Smith <scsmithr@gmail.com>
2024-04-18 16:51:34 -0500

    scalar arith

--- crates/rayexec_bullet/src/compute/arith.rs
+++ crates/rayexec_bullet/src/compute/arith.rs
@@ -1,7 +1,8 @@
 use crate::array::{Array, PrimitiveArray};
+use crate::scalar::ScalarValue;
 use crate::storage::PrimitiveStorage;
 use rayexec_error::{RayexecError, Result};
-use std::ops::{Add, Div, Mul, Rem, Sub};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign};
 
 /// Arithmetic operations that assign the result into the left-hand side.
 pub trait ArithAssign<Rhs = Self> {
@@ -20,7 +21,7 @@ pub trait ArithAssign<Rhs = Self> {
     fn rem(&mut self, right: &Rhs) -> Result<()>;
 }
 
-macro_rules! arith_dispatch {
+macro_rules! array_arith_dispatch {
     ($left:ident, $right:ident, $fn:expr) => {{
         match ($left, $right) {
             (Array::Float32(left), Array::Float32(right)) => $fn(left, right),
@@ -38,43 +39,116 @@ macro_rules! arith_dispatch {
 
 impl ArithAssign for Array {
     fn add(&mut self, right: &Self) -> Result<()> {
-        arith_dispatch!(self, right, ArithAssign::add)
+        array_arith_dispatch!(self, right, ArithAssign::add)
     }
 
     fn checked_add(&mut self, right: &Self) -> Result<()> {
         // TODO
-        arith_dispatch!(self, right, ArithAssign::add)
+        array_arith_dispatch!(self, right, ArithAssign::add)
     }
 
     fn sub(&mut self, right: &Self) -> Result<()> {
-        arith_dispatch!(self, right, ArithAssign::sub)
+        array_arith_dispatch!(self, right, ArithAssign::sub)
     }
 
     fn checked_sub(&mut self, right: &Self) -> Result<()> {
         // TODO
-        arith_dispatch!(self, right, ArithAssign::sub)
+        array_arith_dispatch!(self, right, ArithAssign::sub)
     }
 
     fn mul(&mut self, right: &Self) -> Result<()> {
-        arith_dispatch!(self, right, ArithAssign::mul)
+        array_arith_dispatch!(self, right, ArithAssign::mul)
     }
 
     fn checked_mul(&mut self, right: &Self) -> Result<()> {
         // TODO
-        arith_dispatch!(self, right, ArithAssign::mul)
+        array_arith_dispatch!(self, right, ArithAssign::mul)
     }
 
     fn div(&mut self, right: &Self) -> Result<()> {
-        arith_dispatch!(self, right, ArithAssign::div)
+        array_arith_dispatch!(self, right, ArithAssign::div)
     }
 
     fn checked_div(&mut self, right: &Self) -> Result<()> {
         // TODO
-        arith_dispatch!(self, right, ArithAssign::div)
+        array_arith_dispatch!(self, right, ArithAssign::div)
     }
 
     fn rem(&mut self, right: &Self) -> Result<()> {
-        arith_dispatch!(self, right, ArithAssign::rem)
+        array_arith_dispatch!(self, right, ArithAssign::rem)
+    }
+}
+
+macro_rules! scalar_arith_dispatch {
+    ($left:ident, $right:ident, $fn:expr) => {{
+        match ($left, $right) {
+            (ScalarValue::Float32(left), ScalarValue::Float32(right)) => $fn(left, right),
+            (ScalarValue::Float64(left), ScalarValue::Float64(right)) => $fn(left, right),
+            (ScalarValue::Int8(left), ScalarValue::Int8(right)) => $fn(left, right),
+            (ScalarValue::Int16(left), ScalarValue::Int16(right)) => $fn(left, right),
+            (ScalarValue::Int32(left), ScalarValue::Int32(right)) => $fn(left, right),
+            (ScalarValue::Int64(left), ScalarValue::Int64(right)) => $fn(left, right),
+            (ScalarValue::UInt8(left), ScalarValue::UInt8(right)) => $fn(left, right),
+            (ScalarValue::UInt16(left), ScalarValue::UInt16(right)) => $fn(left, right),
+            (ScalarValue::UInt32(left), ScalarValue::UInt32(right)) => $fn(left, right),
+            (ScalarValue::UInt64(left), ScalarValue::UInt64(right)) => $fn(left, right),
+            _ => Err(RayexecError::new(
+                "Unsupported arithmetic operation on array",
+            )),
+        }
+    }};
+}
+
+impl<'a> ArithAssign for ScalarValue<'a> {
+    fn add(&mut self, right: &Self) -> Result<()> {
+        scalar_arith_dispatch!(self, right, |l, r| {
+            AddAssign::add_assign(l, r);
+            Ok(())
+        })
+    }
+
+    fn checked_add(&mut self, right: &Self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn sub(&mut self, right: &Self) -> Result<()> {
+        scalar_arith_dispatch!(self, right, |l, r| {
+            SubAssign::sub_assign(l, r);
+            Ok(())
+        })
+    }
+
+    fn checked_sub(&mut self, right: &Self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn mul(&mut self, right: &Self) -> Result<()> {
+        scalar_arith_dispatch!(self, right, |l, r| {
+            MulAssign::mul_assign(l, r);
+            Ok(())
+        })
+    }
+
+    fn checked_mul(&mut self, right: &Self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn div(&mut self, right: &Self) -> Result<()> {
+        scalar_arith_dispatch!(self, right, |l, r| {
+            DivAssign::div_assign(l, r);
+            Ok(())
+        })
+    }
+
+    fn checked_div(&mut self, right: &Self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn rem(&mut self, right: &Self) -> Result<()> {
+        scalar_arith_dispatch!(self, right, |l, r| {
+            RemAssign::rem_assign(l, r);
+            Ok(())
+        })
     }
 }
 
@@ -152,4 +226,21 @@ where
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_scalar_add() {
+        // Sanity check
+
+        let mut left = ScalarValue::Int32(5);
+        let right = ScalarValue::Int32(8);
+
+        left.add(&right).unwrap();
+
+        assert_eq!(ScalarValue::Int32(13), left);
+    }
 }
