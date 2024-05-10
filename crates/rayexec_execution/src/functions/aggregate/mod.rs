405d00446f67bd8fd317f68ec1c183f117316a54
Sean Smith <scsmithr@gmail.com>
2024-05-10 14:52:53 -0500

    combine states

--- crates/rayexec_execution/src/functions/aggregate/mod.rs
+++ crates/rayexec_execution/src/functions/aggregate/mod.rs
@@ -3,7 +3,8 @@ pub mod numeric;
 use dyn_clone::DynClone;
 use once_cell::sync::Lazy;
 use rayexec_bullet::{array::Array, executor::aggregate::AggregateState, field::DataType};
-use rayexec_error::Result;
+use rayexec_error::{RayexecError, Result};
+use std::any::Any;
 use std::{fmt::Debug, marker::PhantomData};
 
 use super::{ReturnType, Signature};
@@ -49,33 +50,81 @@ pub trait SpecializedAggregateFunction: Debug + Sync + Send + DynClone {
     fn new_grouped_state(&self) -> Box<dyn GroupedStates>;
 }
 
-// TODO: Combine
 pub trait GroupedStates {
+    /// Needed to allow downcasting to the concrete type when combining multiple
+    /// states that were computed in parallel.
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+
+    /// Generate a new state for a never before seen group in an aggregate.
+    ///
+    /// Returns the index of the newly initialized state that can be used to
+    /// reference the state.
     fn new_group(&mut self) -> usize;
+
+    /// Get the number of group states we're tracking.
+    fn num_groups(&self) -> usize;
+
+    /// Updates states for groups using the provided inputs.
+    ///
+    /// Each row in `inputs` corresponds to values that should be used to update
+    /// states.
+    ///
+    /// `mapping` provides a mapping from the input row to the state that should
+    /// be updated. The 'n'th row in the input corresponds to the 'n'th value in
+    /// `mapping` which corresponds to the state to be updated with the 'n'th
+    /// row.
     fn update_from_arrays(&mut self, inputs: &[Array], mapping: &[usize]) -> Result<()>;
+
+    /// Try to combine two sets of grouped states into a single set of states.
+    ///
+    /// Errors if the concrete types do not match. Essentially this prevents
+    /// trying to combine state between different aggregates (SumI32 and AvgF32)
+    /// _and_ type (SumI32 and SumI64).
+    // TODO: Mapping
+    fn try_combine(&mut self, other: Box<dyn GroupedStates>) -> Result<()>;
+
+    /// Finalizes the aggregate states into a single array.
     fn finalize(&mut self) -> Result<Array>;
 }
 
-pub struct DefaultGroupedStates<S, T, O, UF, FF> {
+/// Provides a default implementation of `GroupedStates`.
+///
+/// Since we're working with multiple aggregates at a time, we need to be able
+/// to box `GroupedStates`, and this type just enables doing that easily.
+///
+/// This essetially provides a wrapping around functions provided by the
+/// aggregate executors, and some number of aggregate states.
+pub struct DefaultGroupedStates<S, T, O, UF, CF, FF> {
+    /// All states we're tracking.
+    ///
+    /// Each state corresponds to a single group.
     states: Vec<S>,
 
+    /// How we should update states given inputs and a mapping array.
     update_fn: UF,
+
+    /// How we should combine states.
+    combine_fn: CF,
+
+    /// How we should finalize the states once we're done updating states.
     finalize_fn: FF,
 
     _t: PhantomData<T>,
     _o: PhantomData<O>,
 }
 
-impl<S, T, O, UF, FF> DefaultGroupedStates<S, T, O, UF, FF>
+impl<S, T, O, UF, CF, FF> DefaultGroupedStates<S, T, O, UF, CF, FF>
 where
     S: AggregateState<T, O>,
     UF: Fn(&[Array], &[usize], &mut [S]) -> Result<()>,
+    CF: Fn(&mut [S], Vec<S>) -> Result<()>,
     FF: Fn(Vec<S>) -> Result<Array>,
 {
-    fn new(update_fn: UF, finalize_fn: FF) -> Self {
+    fn new(update_fn: UF, combine_fn: CF, finalize_fn: FF) -> Self {
         DefaultGroupedStates {
             states: Vec::new(),
             update_fn,
+            combine_fn,
             finalize_fn,
             _t: PhantomData,
             _o: PhantomData,
@@ -83,20 +132,48 @@ where
     }
 }
 
-impl<S, T, O, UF, FF> GroupedStates for DefaultGroupedStates<S, T, O, UF, FF>
+impl<S, T, O, UF, CF, FF> GroupedStates for DefaultGroupedStates<S, T, O, UF, CF, FF>
 where
-    S: AggregateState<T, O>,
-    UF: Fn(&[Array], &[usize], &mut [S]) -> Result<()>,
-    FF: Fn(Vec<S>) -> Result<Array>,
+    T: 'static,
+    O: 'static,
+    S: AggregateState<T, O> + 'static,
+    UF: Fn(&[Array], &[usize], &mut [S]) -> Result<()> + 'static,
+    CF: Fn(&mut [S], Vec<S>) -> Result<()> + 'static,
+    FF: Fn(Vec<S>) -> Result<Array> + 'static,
 {
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+
     fn new_group(&mut self) -> usize {
         let idx = self.states.len();
         self.states.push(S::default());
         idx
     }
 
+    fn num_groups(&self) -> usize {
+        self.states.len()
+    }
+
     fn update_from_arrays(&mut self, inputs: &[Array], mapping: &[usize]) -> Result<()> {
         (self.update_fn)(inputs, mapping, &mut self.states)
+    }
+
+    fn try_combine(&mut self, mut other: Box<dyn GroupedStates>) -> Result<()> {
+        let other = match other.as_any_mut().downcast_mut::<Self>() {
+            Some(other) => other,
+            None => {
+                return Err(RayexecError::new(
+                    "Attempted to combine aggregate states of different types",
+                ))
+            }
+        };
+
+        // TODO: Mapping
+
+        let consume = std::mem::take(&mut other.states);
+
+        (self.combine_fn)(&mut self.states, consume)
     }
 
     fn finalize(&mut self) -> Result<Array> {
--- crates/rayexec_execution/src/functions/aggregate/numeric.rs
+++ crates/rayexec_execution/src/functions/aggregate/numeric.rs
@@ -1,6 +1,6 @@
 use rayexec_bullet::{
     array::{Array, PrimitiveArrayBuilder},
-    executor::aggregate::{AggregateState, StateFinalizer, UnaryUpdater},
+    executor::aggregate::{AggregateState, StateCombiner, StateFinalizer, UnaryUpdater},
     field::DataType,
 };
 
@@ -40,7 +40,7 @@ impl SpecializedAggregateFunction for SumI64 {
     fn new_grouped_state(&self) -> Box<dyn GroupedStates> {
         let update_fn = |arrays: &[Array], mapping: &[usize], states: &mut [SumI64State]| {
             let inputs = match &arrays[0] {
-                Array::Int32(arr) => arr,
+                Array::Int64(arr) => arr,
                 other => panic!("unexpected array type: {other:?}"),
             };
             UnaryUpdater::update(inputs, mapping, states)
@@ -52,7 +52,11 @@ impl SpecializedAggregateFunction for SumI64 {
             Ok(Array::Int64(builder.into_typed_array()))
         };
 
-        Box::new(DefaultGroupedStates::new(update_fn, finalize_fn))
+        Box::new(DefaultGroupedStates::new(
+            update_fn,
+            StateCombiner::combine,
+            finalize_fn,
+        ))
     }
 }
 
@@ -61,18 +65,115 @@ pub struct SumI64State {
     sum: i64,
 }
 
-impl AggregateState<i32, i64> for SumI64State {
+impl AggregateState<i64, i64> for SumI64State {
     fn merge(&mut self, other: Self) -> Result<()> {
-        self.sum = other.sum;
+        self.sum += other.sum;
         Ok(())
     }
 
-    fn update(&mut self, input: i32) -> Result<()> {
-        self.sum += input as i64;
+    fn update(&mut self, input: i64) -> Result<()> {
+        self.sum += input;
         Ok(())
     }
 
     fn finalize(self) -> Result<i64> {
         Ok(self.sum)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rayexec_bullet::array::Int64Array;
+
+    use super::*;
+
+    #[test]
+    fn sum_i32_single_group_two_partitions() {
+        // Single group, two partitions, 'SELECT SUM(a) FROM table'
+
+        let partition_1_vals = Array::Int64(Int64Array::from_iter([1, 2, 3]));
+        let partition_2_vals = Array::Int64(Int64Array::from_iter([4, 5, 6]));
+
+        let specialized = Sum.specialize(&[DataType::Int64]).unwrap();
+
+        let mut states_1 = specialized.new_grouped_state();
+        let mut states_2 = specialized.new_grouped_state();
+
+        let idx_1 = states_1.new_group();
+        assert_eq!(0, idx_1);
+
+        let idx_2 = states_2.new_group();
+        assert_eq!(0, idx_2);
+
+        // All inputs map to the same group (no GROUP BY clause)
+        let mapping_1 = vec![0; partition_1_vals.len()];
+        let mapping_2 = vec![0; partition_2_vals.len()];
+
+        states_1
+            .update_from_arrays(&[partition_1_vals], &mapping_1)
+            .unwrap();
+        states_2
+            .update_from_arrays(&[partition_2_vals], &mapping_2)
+            .unwrap();
+
+        // Combine states.
+        states_1.try_combine(states_2).unwrap();
+
+        // Get final output.
+        let out = states_1.finalize().unwrap();
+        let expected = Array::Int64(Int64Array::from_iter([21]));
+        assert_eq!(expected, out);
+    }
+
+    #[test]
+    fn sum_i32_two_groups_two_partitions() {
+        // Two groups, two partitions, 'SELECT SUM(col2) FROM table GROUP BY col1'
+        //
+        // | col1 | col2 |
+        // |------|------|
+        // | 'a'  | 1    |
+        // | 'a'  | 2    |
+        // | 'b'  | 3    |
+        // | 'b'  | 4    |
+        // | 'b'  | 5    |
+        // | 'a'  | 6    |
+        //
+        // Partition values and mappings represent the positions of the above
+        // table. The actual grouping values are stored in the operator, and
+        // operator is what computes the mappings.
+        let partition_1_vals = Array::Int64(Int64Array::from_iter([1, 2, 3]));
+        let partition_2_vals = Array::Int64(Int64Array::from_iter([4, 5, 6]));
+
+        let specialized = Sum.specialize(&[DataType::Int64]).unwrap();
+
+        let mut states_1 = specialized.new_grouped_state();
+        let mut states_2 = specialized.new_grouped_state();
+
+        // Both partitions are operating on two groups ('a' and 'b').
+        states_1.new_group();
+        states_1.new_group();
+
+        states_2.new_group();
+        states_2.new_group();
+
+        // Mapping corresponding to the above table. Group 'a' == 0 and group
+        // 'b' == 1.
+        let mapping_1 = vec![0, 0, 1];
+        let mapping_2 = vec![1, 1, 0];
+
+        states_1
+            .update_from_arrays(&[partition_1_vals], &mapping_1)
+            .unwrap();
+        states_2
+            .update_from_arrays(&[partition_2_vals], &mapping_2)
+            .unwrap();
+
+        // Combine states.
+        states_1.try_combine(states_2).unwrap();
+
+        // Get final output.
+        let out = states_1.finalize().unwrap();
+        let expected = Array::Int64(Int64Array::from_iter([9, 12]));
+        assert_eq!(expected, out);
     }
 }
