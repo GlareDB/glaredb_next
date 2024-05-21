2c9c7f0cde92b7438b689c620146f74bea3c9061
Sean Smith <scsmithr@gmail.com>
2024-05-21 14:41:54 -0500

    limit impl

--- crates/rayexec_execution/src/execution/operators/limit.rs
+++ crates/rayexec_execution/src/execution/operators/limit.rs
@@ -1,4 +1,5 @@
 use rayexec_bullet::batch::Batch;
+use rayexec_bullet::compute;
 use rayexec_error::{RayexecError, Result};
 use std::task::{Context, Waker};
 
@@ -6,11 +7,13 @@ use super::{OperatorState, PartitionState, PhysicalOperator, PollPull, PollPush}
 
 #[derive(Debug)]
 pub struct LimitPartitionState {
-    /// Remaining offset before we can actually start sending batches.
+    /// Remaining offset before we can actually start sending rows.
     remaining_offset: usize,
 
-    /// Remaining limit before we stop sending batches.
-    remaining_limit: usize,
+    /// Remaining number of rows before we stop sending batches.
+    ///
+    /// Initialized to the operator `limit`.
+    remaining_count: usize,
 
     /// A buffered batch.
     buffer: Option<Batch>,
@@ -50,7 +53,7 @@ impl PhysicalLimit {
     pub fn create_states(&self, partitions: usize) -> Vec<LimitPartitionState> {
         (0..partitions)
             .map(|_| LimitPartitionState {
-                remaining_limit: self.limit,
+                remaining_count: self.limit,
                 remaining_offset: self.offset.unwrap_or(0),
                 buffer: None,
                 pull_waker: None,
@@ -64,28 +67,105 @@ impl PhysicalLimit {
 impl PhysicalOperator for PhysicalLimit {
     fn poll_push(
         &self,
-        _cx: &mut Context,
-        _partition_state: &mut PartitionState,
+        cx: &mut Context,
+        partition_state: &mut PartitionState,
         _operator_state: &OperatorState,
-        _batch: Batch,
+        batch: Batch,
     ) -> Result<PollPush> {
-        unimplemented!()
+        let state = match partition_state {
+            PartitionState::Limit(state) => state,
+            other => panic!("invalid partition state: {other:?}"),
+        };
+
+        if state.buffer.is_some() {
+            state.push_waker = Some(cx.waker().clone());
+            return Ok(PollPush::Pending(batch));
+        }
+
+        // We're done, no more inputs should arrive.
+        if state.remaining_count == 0 {
+            // TODO: Double check that partition pipeline calls
+            // `finalize_push` after receiving this.
+            return Ok(PollPush::Break);
+        }
+
+        let batch = if state.remaining_offset > 0 {
+            let count = std::cmp::min(
+                batch.num_rows(),
+                state.remaining_count + state.remaining_offset,
+            );
+            let cols = batch
+                .columns()
+                .iter()
+                .map(|arr| compute::slice::slice(arr.as_ref(), state.remaining_offset, count))
+                .collect::<Result<Vec<_>>>()?;
+            let batch = Batch::try_new(cols)?;
+            state.remaining_offset = 0;
+            state.remaining_count -= batch.num_rows();
+            batch
+        } else if state.remaining_count < batch.num_rows() {
+            let cols = batch
+                .columns()
+                .iter()
+                .map(|arr| compute::slice::slice(arr.as_ref(), 0, state.remaining_count))
+                .collect::<Result<Vec<_>>>()?;
+            let batch = Batch::try_new(cols)?;
+            state.remaining_count = 0;
+            batch
+        } else {
+            state.remaining_count -= batch.num_rows();
+            batch
+        };
+
+        state.buffer = Some(batch);
+        if let Some(waker) = state.pull_waker.take() {
+            waker.wake();
+        }
+
+        Ok(PollPush::Pushed)
     }
 
     fn finalize_push(
         &self,
-        _partition_state: &mut PartitionState,
+        partition_state: &mut PartitionState,
         _operator_state: &OperatorState,
     ) -> Result<()> {
-        unimplemented!()
+        let state = match partition_state {
+            PartitionState::Limit(state) => state,
+            other => panic!("invalid partition state: {other:?}"),
+        };
+
+        state.finished = true;
+        if let Some(waker) = state.pull_waker.take() {
+            waker.wake();
+        }
+
+        Ok(())
     }
 
     fn poll_pull(
         &self,
-        _cx: &mut Context,
+        cx: &mut Context,
         partition_state: &mut PartitionState,
         _operator_state: &OperatorState,
     ) -> Result<PollPull> {
-        unimplemented!()
+        let state = match partition_state {
+            PartitionState::Limit(state) => state,
+            other => panic!("invalid partition state: {other:?}"),
+        };
+
+        match state.buffer.take() {
+            Some(batch) => Ok(PollPull::Batch(batch)),
+            None => {
+                if state.finished {
+                    return Ok(PollPull::Exhausted);
+                }
+                state.pull_waker = Some(cx.waker().clone());
+                if let Some(waker) = state.push_waker.take() {
+                    waker.wake();
+                }
+                Ok(PollPull::Pending)
+            }
+        }
     }
 }
