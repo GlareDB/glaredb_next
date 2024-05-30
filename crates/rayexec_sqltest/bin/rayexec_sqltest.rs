14f8ae87e7e462fd1e6df0c34ce4828e9a958ec4
Sean Smith <scsmithr@gmail.com>
2024-05-30 13:06:24 -0500

    use libtest mimic

--- crates/rayexec_sqltest/bin/rayexec_sqltest.rs
+++ crates/rayexec_sqltest/bin/rayexec_sqltest.rs
@@ -1,39 +1,20 @@
 //! Bin for running the SLTs.
 
-use clap::Parser;
+use libtest_mimic::{Arguments, Trial};
 use rayexec_error::{Result, ResultExt};
 use rayexec_sqltest::run_test;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use tokio::runtime::Builder;
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::FmtSubscriber;
 
 /// Path to slts directory relative to this crate's root.
 const SLTS_PATH: &str = "slts/";
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Arguments {
-    /// Only test files that contain this filter string are run.
-    // Positional
-    #[clap(value_name = "FILTER")]
-    pub filter: Option<String>,
-}
-
-impl Arguments {
-    /// Filter file paths based on the filter provided as an argument.
-    fn filter_paths(&self, mut paths: Vec<PathBuf>) -> Vec<PathBuf> {
-        paths.retain(|path| match self.filter.as_ref() {
-            Some(filter) => path.to_string_lossy().contains(filter),
-            None => true,
-        });
-        paths
-    }
-}
-
-#[tokio::main(flavor = "current_thread")]
-pub async fn main() {
-    let args = Arguments::parse();
+pub fn main() {
+    let args = Arguments::from_args();
 
     let env_filter = EnvFilter::builder()
         .with_default_directive(tracing::Level::INFO.into())
@@ -44,8 +25,6 @@ pub async fn main() {
     let subscriber = FmtSubscriber::builder()
         .with_test_writer() // TODO: Actually capture
         .with_env_filter(env_filter)
-        .with_thread_ids(true)
-        .with_thread_names(true)
         .with_file(true)
         .with_line_number(true)
         .finish();
@@ -60,19 +39,27 @@ pub async fn main() {
     let mut paths = Vec::new();
     find_files(Path::new(SLTS_PATH), &mut paths).unwrap();
 
-    paths = args.filter_paths(paths);
+    let rt = Arc::new(
+        Builder::new_current_thread()
+            .enable_all()
+            .thread_name("rayexec_sqltest")
+            .build()
+            .unwrap(),
+    );
 
-    for path in paths {
-        if let Err(e) = run_test(&path).await {
-            println!("---- FAIL ----");
-            println!("{e}");
-            println!(
-                "Rerun this SLT with 'cargo test -p rayexec_sqltest {}'",
-                path.to_string_lossy()
-            );
-            std::process::exit(1);
-        }
-    }
+    let tests = paths
+        .into_iter()
+        .map(|path| {
+            let test_name = path.to_string_lossy().to_string();
+            let rt = rt.clone();
+            Trial::test(test_name, move || match rt.block_on(run_test(path)) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.into()),
+            })
+        })
+        .collect();
+
+    libtest_mimic::run(&args, tests).exit();
 }
 
 fn find_files(dir: &Path, paths: &mut Vec<PathBuf>) -> Result<()> {
