0ad917de1102da64981504fe47d20306ada9676c
Sean Smith <scsmithr@gmail.com>
2024-06-03 14:08:04 -0500

    attach parse

---
+++ crates/rayexec_parser/src/ast/attach.rs
@@ -0,0 +1,121 @@
+use std::collections::HashMap;
+use rayexec_error::{ Result, RayexecError };
+
+use crate::{keywords::Keyword, parser::Parser, tokens::Token};
+
+use super::{AstParseable, Expr, Ident, ObjectReference};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AttachType {
+    Database,
+    Table,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Attach {
+    pub datasource_name: Ident,
+    pub attach_type: AttachType,
+    pub dest: ObjectReference,
+    pub options: HashMap<Ident, Expr>,
+}
+
+impl AstParseable for Attach {
+    fn parse(parser: &mut Parser) -> Result<Self> {
+        parser.expect_keyword(Keyword::ATTACH)?;
+        let datasource_name = Ident::parse(parser)?;
+
+        let attach_type = match parser.next_keyword()? {
+            Keyword::DATABASE => AttachType::Database,
+            Keyword::TABLE => AttachType::Table,
+            other => {
+                return Err(RayexecError::new(format!(
+                    "Expected DATABASE or TABLE for attach type, got '{other}'"
+                )))
+            }
+        };
+
+        let dest = ObjectReference::parse(parser)?;
+
+        let mut options = HashMap::new();
+        loop {
+            let key = match Ident::parse(parser) {
+                Ok(ident) => ident,
+                Err(_) => break,
+            };
+
+            parser.expect_token(&Token::Eq)?;
+
+            let val = Expr::parse(parser)?;
+            options.insert(key, val);
+
+            if !parser.consume_token(&Token::Comma) {
+                break;
+            }
+        }
+
+        Ok(Attach {
+            datasource_name,
+            attach_type,
+            dest,
+            options,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ast::{testutil::parse_ast, Literal};
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn attach_pg_database() {
+        let got = parse_ast::<Attach>("ATTACH POSTGRES DATABASE my_pg CONNECTION_STRING = 'postgres://sean:pass@localhost/db'").unwrap();
+        let expected = Attach {
+            datasource_name: Ident::from_string("POSTGRES"),
+            attach_type: AttachType::Database,
+            dest: ObjectReference::from_strings(["my_pg"]),
+            options: [(
+                Ident::from_string("CONNECTION_STRING"),
+                Expr::Literal(Literal::SingleQuotedString(
+                    "postgres://sean:pass@localhost/db".to_string(),
+                )),
+            )]
+            .into_iter()
+            .collect(),
+        };
+
+        assert_eq!(expected, got);
+    }
+
+    #[test]
+    fn attach_pg_table() {
+        let got = parse_ast::<Attach>("ATTACH POSTGRES TABLE my_pg_table CONNECTION_STRING = 'postgres://sean:pass@localhost/db', SCHEMA = 'public', TABLE = 'users'").unwrap();
+        let expected = Attach {
+            datasource_name: Ident::from_string("POSTGRES"),
+            attach_type: AttachType::Table,
+            dest: ObjectReference::from_strings(["my_pg_table"]),
+            options: [
+                (
+                    Ident::from_string("CONNECTION_STRING"),
+                    Expr::Literal(Literal::SingleQuotedString(
+                        "postgres://sean:pass@localhost/db".to_string(),
+                    )),
+                ),
+                (
+                    Ident::from_string("SCHEMA"),
+                    Expr::Literal(Literal::SingleQuotedString("public".to_string())),
+                ),
+                (
+                    Ident::from_string("TABLE"),
+                    Expr::Literal(Literal::SingleQuotedString("users".to_string())),
+                ),
+            ]
+            .into_iter()
+            .collect(),
+        };
+
+        assert_eq!(expected, got);
+    }
+}
