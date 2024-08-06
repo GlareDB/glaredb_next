aee1740bfdb0ad7a54ba467766a8b33e654325f9
Sean Smith <scsmithr@gmail.com>
2024-08-06 18:01:50 -0500

    fixup! protoc ci

--- scripts/install_protoc_linux.sh
+++ scripts/install_protoc_linux.sh
@@ -2,4 +2,4 @@
 
 curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v23.1/protoc-23.1-linux-x86_64.zip" -o protoc.zip
 unzip -o protoc.zip
-mv bin/protoc /bin/.
+mv bin/protoc /bin
