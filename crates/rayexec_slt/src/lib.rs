3f3e663781adaa2acd003e65b510a10a1d2203e7
Sean Smith <scsmithr@gmail.com>
2024-08-18 09:38:06 -0500

    split

--- crates/rayexec_slt/src/lib.rs
+++ crates/rayexec_slt/src/lib.rs
@@ -1,13 +1,11 @@
+mod convert;
+mod vars;
+
 use async_trait::async_trait;
+use convert::{batch_to_rows, schema_to_types};
 use futures::StreamExt;
 use libtest_mimic::{Arguments, Trial};
 use rand::{distributions::Alphanumeric, Rng};
-use rayexec_bullet::{
-    batch::Batch,
-    datatype::DataType,
-    field::Schema,
-    format::{FormatOptions, Formatter},
-};
 use rayexec_error::{RayexecError, Result, ResultExt};
 use rayexec_execution::{
     engine::{session::Session, Engine},
@@ -23,126 +21,7 @@ use std::{
 };
 use tracing::info;
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
-
-#[derive(Clone)]
-pub enum VarValue {
-    /// Value is sensitive, don't print it out.
-    Sensitive(String),
-    /// Value is not sensitive, print it during debugging.
-    Plain(String),
-}
-
-impl VarValue {
-    pub fn plain_from_env(key: &str) -> VarValue {
-        match std::env::var(key) {
-            Ok(s) => VarValue::Plain(s),
-            Err(_) => {
-                println!("Missing environment variable: {key}");
-                std::process::exit(2);
-            }
-        }
-    }
-
-    pub fn sensitive_from_env(key: &str) -> VarValue {
-        match std::env::var(key) {
-            Ok(s) => VarValue::Sensitive(s),
-            Err(_) => {
-                println!("Missing environment variable: {key}");
-                std::process::exit(2);
-            }
-        }
-    }
-}
-
-impl fmt::Display for VarValue {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Sensitive(_) => write!(f, "***"),
-            VarValue::Plain(s) => write!(f, "{s}"),
-        }
-    }
-}
-
-impl fmt::Debug for VarValue {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{self}")
-    }
-}
-
-impl AsRef<str> for VarValue {
-    fn as_ref(&self) -> &str {
-        match self {
-            Self::Sensitive(s) => s.as_str(),
-            VarValue::Plain(s) => s.as_str(),
-        }
-    }
-}
-
-/// Variables that can be referenced in sql queries and automatically replaced
-/// with concrete values.
-///
-/// Variable format in sql queries: __MYVARIABLE__
-///
-/// When adding a variable, they'll automatically be uppercased and surrounded
-/// with understores.
-///
-/// See default implementation for predefined variables.
-///
-/// A new instance of these variables is created for each file run.
-#[derive(Debug, Clone)]
-pub struct ReplacementVars {
-    vars: HashMap<String, VarValue>,
-}
-
-impl Default for ReplacementVars {
-    fn default() -> Self {
-        let mut vars = ReplacementVars {
-            vars: HashMap::new(),
-        };
-
-        let s: String = rand::thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(7)
-            .map(char::from)
-            .collect();
-
-        // Dir (relative to test_bin) where tests can write temp files for
-        // things like COPY TO.
-        vars.add_var("SLT_TMP", VarValue::Plain(format!("../slt_tmp/{s}")));
-
-        vars
-    }
-}
-
-impl ReplacementVars {
-    pub fn add_var(&mut self, key: &str, val: VarValue) {
-        let key = format!("__{}__", key.to_uppercase());
-        self.vars.insert(key, val);
-    }
-
-    fn replace_in_query(&self, query: impl Into<String>, conf: &RunConfig) -> Result<String> {
-        let mut query = query.into();
-
-        for (k, v) in &self.vars {
-            if k == "__SLT_TMP__" && conf.create_slt_tmp {
-                std::fs::create_dir_all(v.as_ref()).context("failed to create slt tmp dir")?
-            }
-
-            query = query.replace(k, v.as_ref());
-        }
-
-        Ok(query)
-    }
-}
-
-impl fmt::Display for ReplacementVars {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for (k, v) in &self.vars {
-            writeln!(f, "{k} = {v}")?;
-        }
-        Ok(())
-    }
-}
+use vars::ReplacementVars;
 
 #[derive(Debug, Clone, Default)]
 pub struct RunConfig {
@@ -311,7 +190,14 @@ where
     ) -> Result<sqllogictest::DBOutput<DefaultColumnType>, RayexecError> {
         info!(%sql, "query");
 
-        let sql = self.conf.vars.replace_in_query(sql, &self.conf)?;
+        let mut sql = sql.to_string();
+        for (k, v) in self.conf.vars.iter() {
+            if k == "__SLT_TMP__" && self.conf.create_slt_tmp {
+                std::fs::create_dir_all(v.as_ref()).context("failed to create slt tmp dir")?
+            }
+
+            sql = sql.replace(k, v.as_ref());
+        }
 
         let mut rows = Vec::new();
         let mut results = self.session.simple(&sql).await?;
@@ -370,150 +256,5 @@ where
 
     fn engine_name(&self) -> &str {
         "rayexec"
-    }
-}
-
-/// Convert a batch into a vector of rows.
-fn batch_to_rows(batch: Batch) -> Result<Vec<Vec<String>>> {
-    const OPTS: FormatOptions = FormatOptions {
-        null: "NULL",
-        empty_string: "(empty)",
-    };
-    let formatter = Formatter::new(OPTS);
-
-    let mut rows = Vec::new();
-
-    for row_idx in 0..batch.num_rows() {
-        let row = batch.row(row_idx).expect("row to exist");
-
-        let col_strings: Vec<_> = row
-            .iter()
-            .map(|col| formatter.format_scalar_value(col.clone()).to_string())
-            .collect();
-
-        match transform_multiline_cols_to_rows(&col_strings) {
-            Some(new_rows) => rows.extend(new_rows),
-            None => rows.push(col_strings),
-        }
-    }
-
-    Ok(rows)
-}
-
-/// Transforms a row with a potentially multiline column into multiple rows with
-/// each column containing a single line.
-///
-/// Columns that don't have content for that row will instead have a '.'.
-///
-/// For example, the following output:
-/// ```text
-/// +-------------+---------------------------------------------+
-/// | type        | plan                                        |
-/// +-------------+---------------------------------------------+
-/// | logical     | Order (expressions = [#0 DESC NULLS FIRST]) |
-/// |             |   Projection (expressions = [#0])           |
-/// |             |     ExpressionList                          |
-/// +-------------+---------------------------------------------+
-/// | pipeline    | Pipeline 1                                  |
-/// |             |   Pipeline 2                                |
-/// +-------------+---------------------------------------------+
-/// ```
-///
-/// Would get trasnformed into:
-/// ```text
-/// logical   Order (expressions = [#0 DESC NULLS FIRST])
-/// .           Projection (expressions = [#0])
-/// .             ExpressionList
-/// pipeline  Pipeline 1
-/// .           Pipeline 2
-/// ```
-/// Where each line is a new "row".
-///
-/// This allows for nicely formatted SLTs for queries that return multiline
-/// results (like EXPLAIN).
-fn transform_multiline_cols_to_rows<S: AsRef<str>>(cols: &[S]) -> Option<Vec<Vec<String>>> {
-    let max = cols.iter().fold(0, |curr, col| {
-        let col_lines = col.as_ref().lines().count();
-        if col_lines > curr {
-            col_lines
-        } else {
-            curr
-        }
-    });
-
-    if max > 1 {
-        let mut new_rows = Vec::new();
-        for row_idx in 0..max {
-            let new_row: Vec<_> = cols
-                .iter()
-                .map(|col| col.as_ref().lines().nth(row_idx).unwrap_or(".").to_string())
-                .collect();
-            new_rows.push(new_row)
-        }
-        Some(new_rows)
-    } else {
-        None
-    }
-}
-
-fn schema_to_types(schema: &Schema) -> Vec<DefaultColumnType> {
-    let mut typs = Vec::new();
-    for field in &schema.fields {
-        let typ = match field.datatype {
-            DataType::Int8
-            | DataType::Int16
-            | DataType::Int32
-            | DataType::Int64
-            | DataType::UInt8
-            | DataType::UInt16
-            | DataType::UInt32
-            | DataType::UInt64 => DefaultColumnType::Integer,
-            DataType::Float32 | DataType::Float64 => DefaultColumnType::FloatingPoint,
-            DataType::Utf8 | DataType::LargeUtf8 | DataType::Boolean => DefaultColumnType::Text,
-            _ => DefaultColumnType::Any,
-        };
-        typs.push(typ);
-    }
-
-    typs
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn transform_not_needed() {
-        let orig_row = &["col1", "col2", "col3"];
-        let out = transform_multiline_cols_to_rows(orig_row);
-        assert_eq!(None, out);
-    }
-
-    #[test]
-    fn transform_multiline_col() {
-        let orig_row = &["col1", "col2\ncol2a\ncol2b", "col3"];
-        let out = transform_multiline_cols_to_rows(orig_row);
-
-        let expected = vec![
-            vec!["col1".to_string(), "col2".to_string(), "col3".to_string()],
-            vec![".".to_string(), "col2a".to_string(), ".".to_string()],
-            vec![".".to_string(), "col2b".to_string(), ".".to_string()],
-        ];
-
-        assert_eq!(Some(expected), out);
-    }
-
-    #[test]
-    fn transform_multiple_multiline_cols() {
-        let orig_row = &["col1", "col2\ncol2a\ncol2b", "col3\ncol3a"];
-        let out = transform_multiline_cols_to_rows(orig_row);
-
-        let expected = vec![
-            vec!["col1".to_string(), "col2".to_string(), "col3".to_string()],
-            vec![".".to_string(), "col2a".to_string(), "col3a".to_string()],
-            vec![".".to_string(), "col2b".to_string(), ".".to_string()],
-        ];
-
-        assert_eq!(Some(expected), out);
     }
 }
