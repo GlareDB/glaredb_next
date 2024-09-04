663f3ea5cb30ba74e59fe608c22ad282a55c63cf
Sean Smith <scsmithr@gmail.com>
2024-09-04 09:19:36 -0400

    fix split conjunction

--- crates/rayexec_execution/src/logical/binder/bind_query/condition_extractor.rs
+++ crates/rayexec_execution/src/logical/binder/bind_query/condition_extractor.rs
@@ -206,6 +206,90 @@ fn split_conjunction(expr: Expression, out: &mut Vec<Expression>) {
         Some(expr)
     }
 
-    let last = inner(expr, out).expect("tail expression");
-    out.push(last)
+    if let Some(expr) = inner(expr, out) {
+        out.push(expr)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rayexec_bullet::scalar::ScalarValue;
+
+    use crate::expr::literal_expr::LiteralExpr;
+
+    use super::*;
+
+    #[test]
+    fn split_conjunction_none() {
+        let expr = Expression::Literal(LiteralExpr {
+            literal: ScalarValue::Int8(4),
+        });
+
+        let mut out = Vec::new();
+        split_conjunction(expr.clone(), &mut out);
+
+        let expected = vec![expr];
+        assert_eq!(expected, out);
+    }
+
+    #[test]
+    fn split_conjunction_single_and() {
+        let expr = Expression::Conjunction(ConjunctionExpr {
+            left: Box::new(Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(true),
+            })),
+            right: Box::new(Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(false),
+            })),
+            op: ConjunctionOperator::And,
+        });
+
+        let mut out = Vec::new();
+        split_conjunction(expr, &mut out);
+
+        let expected = vec![
+            Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(true),
+            }),
+            Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(false),
+            }),
+        ];
+        assert_eq!(expected, out);
+    }
+
+    #[test]
+    fn split_conjunction_nested_and() {
+        let expr = Expression::Conjunction(ConjunctionExpr {
+            left: Box::new(Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(true),
+            })),
+            right: Box::new(Expression::Conjunction(ConjunctionExpr {
+                left: Box::new(Expression::Literal(LiteralExpr {
+                    literal: ScalarValue::Boolean(true),
+                })),
+                right: Box::new(Expression::Literal(LiteralExpr {
+                    literal: ScalarValue::Boolean(false),
+                })),
+                op: ConjunctionOperator::And,
+            })),
+            op: ConjunctionOperator::And,
+        });
+
+        let mut out = Vec::new();
+        split_conjunction(expr, &mut out);
+
+        let expected = vec![
+            Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(true),
+            }),
+            Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(true),
+            }),
+            Expression::Literal(LiteralExpr {
+                literal: ScalarValue::Boolean(false),
+            }),
+        ];
+        assert_eq!(expected, out);
+    }
 }
