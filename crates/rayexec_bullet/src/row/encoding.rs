3bc9c395373434755c9f94bc340f0b4f3e9e9c90
Sean Smith <scsmithr@gmail.com>
2024-09-17 10:24:25 -0500

    tests, clarity

--- crates/rayexec_bullet/src/row/encoding.rs
+++ crates/rayexec_bullet/src/row/encoding.rs
@@ -6,7 +6,6 @@ use rayexec_error::{not_implemented, RayexecError, Result};
 pub struct ComparableRows {
     /// Underlying row data.
     data: Vec<u8>,
-
     /// Offsets into the data buffer.
     offsets: Vec<usize>,
 }
@@ -81,23 +80,34 @@ impl<'a> ComparableRow<'a> {
     }
 }
 
+/// Configuration for how to encode a column.
 #[derive(Debug, Clone)]
 pub struct ComparableColumn {
+    /// If we should encode columns to reverse the natural sort order for
+    /// values.
+    ///
+    /// If this is false, this should correspond to sorting in ascending order
+    /// (e.g. '1 < 2' evaluates to true).
+    ///
+    /// If true, this is reverse the sort order (e.g. '1 < 2' evaluates to
+    /// false, causing '2' to come before '1').
     pub desc: bool,
+    /// If we should encode nulls such that they should be ordered before any
+    /// valid values.
     pub nulls_first: bool,
 }
 
 impl ComparableColumn {
-    const fn null_bit(&self) -> u8 {
+    const fn null_byte(&self) -> u8 {
         if self.nulls_first {
-            1
-        } else {
             0
+        } else {
+            0xFF
         }
     }
 
-    const fn valid_bit(&self) -> u8 {
-        1 - self.null_bit()
+    const fn valid_byte(&self) -> u8 {
+        !self.null_byte()
     }
 
     /// Invert all bits in buf if this column should be ordered descending.
@@ -326,11 +336,11 @@ impl ComparableRowEncoder {
         buf: &mut [u8],
         start: usize,
     ) -> usize {
-        let null_bit = col.null_bit();
-        let valid_bit = col.valid_bit();
+        let null_byte = col.null_byte();
+        let valid_byte = col.valid_byte();
 
         if arr.is_valid(row).expect("row to be in bounds") {
-            buf[start] = valid_bit;
+            buf[start] = valid_byte;
             let value = arr.value(row).expect("row to be in bounds");
             let end = start + 1 + value.as_binary().len();
             let write_buf = &mut buf[start + 1..end];
@@ -339,7 +349,7 @@ impl ComparableRowEncoder {
 
             start + 1 + write_buf.len()
         } else {
-            buf[start] = null_bit;
+            buf[start] = null_byte;
 
             start + 1
         }
@@ -355,11 +365,11 @@ impl ComparableRowEncoder {
         buf: &mut [u8],
         start: usize,
     ) -> usize {
-        let null_bit = col.null_bit();
-        let valid_bit = col.valid_bit();
+        let null_byte = col.null_byte();
+        let valid_byte = col.valid_byte();
 
         if arr.is_valid(row).expect("row to be in bounds") {
-            buf[start] = valid_bit;
+            buf[start] = valid_byte;
             let value = arr.value(row).expect("row to be in bounds");
             let end = start + 1 + std::mem::size_of::<T>();
             let write_buf = &mut buf[start + 1..end];
@@ -368,7 +378,7 @@ impl ComparableRowEncoder {
 
             start + 1 + write_buf.len()
         } else {
-            buf[start] = null_bit;
+            buf[start] = null_byte;
 
             start + 1
         }
@@ -533,5 +543,101 @@ mod tests {
 
         let expected = vec![Ordering::Less, Ordering::Greater, Ordering::Equal];
         assert_eq!(expected, cmps);
+    }
+
+    #[test]
+    fn primitive_nulls_last_asc() {
+        let col1 = Array::Int32(Int32Array::from_iter([Some(-1), None, Some(1), Some(2)]));
+        let col2 = Array::Int32(Int32Array::from_iter([Some(1), Some(0), Some(-1), None]));
+
+        let encoder = ComparableRowEncoder {
+            columns: vec![ComparableColumn {
+                desc: false,
+                nulls_first: false,
+            }],
+        };
+
+        let rows1 = encoder.encode(&[&col1]).unwrap();
+        let rows2 = encoder.encode(&[&col2]).unwrap();
+
+        assert_eq!(4, rows1.num_rows());
+        assert_eq!(4, rows2.num_rows());
+
+        assert!(rows1.row(0).unwrap() < rows2.row(0).unwrap());
+        assert!(rows1.row(1).unwrap() > rows2.row(1).unwrap());
+        assert!(rows1.row(2).unwrap() > rows2.row(2).unwrap());
+        assert!(rows1.row(3).unwrap() < rows2.row(3).unwrap());
+    }
+
+    #[test]
+    fn primitive_nulls_last_desc() {
+        let col1 = Array::Int32(Int32Array::from_iter([Some(-1), None, Some(1), Some(2)]));
+        let col2 = Array::Int32(Int32Array::from_iter([Some(1), Some(0), Some(-1), None]));
+
+        let encoder = ComparableRowEncoder {
+            columns: vec![ComparableColumn {
+                desc: true,
+                nulls_first: false,
+            }],
+        };
+
+        let rows1 = encoder.encode(&[&col1]).unwrap();
+        let rows2 = encoder.encode(&[&col2]).unwrap();
+
+        assert_eq!(4, rows1.num_rows());
+        assert_eq!(4, rows2.num_rows());
+
+        assert!(rows1.row(0).unwrap() > rows2.row(0).unwrap());
+        assert!(rows1.row(1).unwrap() > rows2.row(1).unwrap());
+        assert!(rows1.row(2).unwrap() < rows2.row(2).unwrap());
+        assert!(rows1.row(3).unwrap() < rows2.row(3).unwrap());
+    }
+
+    #[test]
+    fn primitive_nulls_first_asc() {
+        let col1 = Array::Int32(Int32Array::from_iter([Some(-1), None, Some(1), Some(2)]));
+        let col2 = Array::Int32(Int32Array::from_iter([Some(1), Some(0), Some(-1), None]));
+
+        let encoder = ComparableRowEncoder {
+            columns: vec![ComparableColumn {
+                desc: false,
+                nulls_first: true,
+            }],
+        };
+
+        let rows1 = encoder.encode(&[&col1]).unwrap();
+        let rows2 = encoder.encode(&[&col2]).unwrap();
+
+        assert_eq!(4, rows1.num_rows());
+        assert_eq!(4, rows2.num_rows());
+
+        assert!(rows1.row(0).unwrap() < rows2.row(0).unwrap());
+        assert!(rows1.row(1).unwrap() < rows2.row(1).unwrap());
+        assert!(rows1.row(2).unwrap() > rows2.row(2).unwrap());
+        assert!(rows1.row(3).unwrap() > rows2.row(3).unwrap());
+    }
+
+    #[test]
+    fn primitive_nulls_first_desc() {
+        let col1 = Array::Int32(Int32Array::from_iter([Some(-1), None, Some(1), Some(2)]));
+        let col2 = Array::Int32(Int32Array::from_iter([Some(1), Some(0), Some(-1), None]));
+
+        let encoder = ComparableRowEncoder {
+            columns: vec![ComparableColumn {
+                desc: true,
+                nulls_first: true,
+            }],
+        };
+
+        let rows1 = encoder.encode(&[&col1]).unwrap();
+        let rows2 = encoder.encode(&[&col2]).unwrap();
+
+        assert_eq!(4, rows1.num_rows());
+        assert_eq!(4, rows2.num_rows());
+
+        assert!(rows1.row(0).unwrap() > rows2.row(0).unwrap());
+        assert!(rows1.row(1).unwrap() < rows2.row(1).unwrap());
+        assert!(rows1.row(2).unwrap() < rows2.row(2).unwrap());
+        assert!(rows1.row(3).unwrap() > rows2.row(3).unwrap());
     }
 }
