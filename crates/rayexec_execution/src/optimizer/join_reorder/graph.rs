7b2af9766c641e3916cc51534c190e36214548c6
Sean Smith <scsmithr@gmail.com>
2024-10-19 10:59:20 -0500

    fixup! sure

--- crates/rayexec_execution/src/optimizer/join_reorder/graph.rs
+++ crates/rayexec_execution/src/optimizer/join_reorder/graph.rs
@@ -4,7 +4,7 @@ use rayexec_error::{RayexecError, Result};
 
 use crate::expr::comparison_expr::ComparisonOperator;
 use crate::logical::binder::bind_context::TableRef;
-use crate::logical::logical_join::JoinType;
+use crate::logical::logical_join::{ComparisonCondition, JoinType};
 use crate::logical::operator::{LogicalNode, LogicalOperator};
 use crate::optimizer::filter_pushdown::condition_extractor::JoinConditionExtractor;
 use crate::optimizer::filter_pushdown::extracted_filter::ExtractedFilter;
@@ -14,12 +14,18 @@ use crate::optimizer::join_reorder::set::{binary_partitions, powerset};
 type RelId = usize;
 
 /// Unique id for indentifying join conditions (edges) in the graph.
-type ConditionId = usize;
+type EdgeId = usize;
+
+/// Unique id for extra filters in the graph.
+type FilterId = usize;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct PlanKey(Vec<RelId>);
 
 impl PlanKey {
+    /// Creates a new plan key from relation ids.
+    ///
+    /// This sorts the ids to ensure they're consistent.
     fn new_from_ids(ids: impl IntoIterator<Item = RelId>) -> Self {
         let mut v: Vec<_> = ids.into_iter().collect();
         v.sort_unstable();
@@ -27,10 +33,62 @@ impl PlanKey {
     }
 }
 
+/// A generated plan represents a either a join between two plan subsets, or a
+/// base relations.
+#[derive(Debug)]
+struct GeneratedPlan {
+    /// Relative cost of executing _this_ plan.
+    cost: u64,
+    /// Output table refs for this plan.
+    ///
+    /// Union of all child output refs.
+    output_refs: HashSet<TableRef>,
+    /// Conditions that should be used when joining left and right.
+    conditions: HashSet<EdgeId>,
+    /// Left input to the plan. Will be None if this plan is for a base relation.
+    left_input: Option<PlanKey>,
+    /// Right input to the plan. Will be None if this plan is for a base relation.
+    right_input: Option<PlanKey>,
+    /// Filters that will be applied to the left input.
+    left_filters: HashSet<FilterId>,
+    /// Filters that will be applied to the right input.
+    right_filters: HashSet<FilterId>,
+    /// Complete set of used edges up to and including this plan.
+    ///
+    /// Union of all edges used in children.
+    ///
+    /// This lets us track which filters/conditions we have used so far when
+    /// considering this join order. We don't want to reuse filters/conditions
+    /// within a join order.
+    used: UsedEdges,
+}
+
+/// Tracks edges that have been used thus far in a particular join ordering.
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+struct UsedEdges {
+    /// Complete set of filters used.
+    filters: HashSet<FilterId>,
+    /// Complete set of edges used.
+    edges: HashSet<EdgeId>,
+}
+
+/// Edge in the graph linking two relations.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Edge {
+    /// The join condition.
+    pub condition: ComparisonCondition,
+    /// Refs on the left side of the comparison.
+    pub left_refs: HashSet<TableRef>,
+    /// Refs on the right side of the comparison.
+    pub right_refs: HashSet<TableRef>,
+}
+
 #[derive(Debug)]
 pub struct Graph {
-    /// Extracted join conditions.
-    conditions: HashMap<ConditionId, ExtractedFilter>,
+    /// Edges in the graph.
+    edge: HashMap<EdgeId, Edge>,
+    /// Extra filters in the graph.
+    filters: HashMap<FilterId, ExtractedFilter>,
     /// Base relations in the graph that we're joining.
     base_relations: HashMap<RelId, LogicalOperator>,
 }
@@ -47,6 +105,12 @@ impl Graph {
                 GeneratedPlan {
                     cost: 0,
                     output_refs: base_rel.get_output_table_refs().into_iter().collect(),
+                    conditions: HashSet::new(),
+                    left_input: None,
+                    right_input: None,
+                    left_filters: HashSet::new(),  // TODO
+                    right_filters: HashSet::new(), // TODO
+                    used: UsedEdges::default(),    // TODO
                 },
             );
         }
@@ -84,15 +148,15 @@ impl Graph {
                         }
                     }
 
-                    best_subset_plan = Some(GeneratedPlan {
-                        cost,
-                        output_refs: p1
-                            .output_refs
-                            .iter()
-                            .copied()
-                            .chain(p2.output_refs.iter().copied())
-                            .collect(),
-                    });
+                    // best_subset_plan = Some(GeneratedPlan {
+                    //     cost,
+                    //     output_refs: p1
+                    //         .output_refs
+                    //         .iter()
+                    //         .copied()
+                    //         .chain(p2.output_refs.iter().copied())
+                    //         .collect(),
+                    // });
                 }
 
                 // Add to best plans.
@@ -151,12 +215,4 @@ impl Graph {
             }
         }
     }
-}
-
-#[derive(Debug)]
-struct GeneratedPlan {
-    /// Relative cost of executing _this_ plan.
-    cost: u64,
-    /// Output table refs for this plan.
-    output_refs: HashSet<TableRef>,
 }
